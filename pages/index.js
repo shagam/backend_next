a0ae263f528ff01e2924f38b83bfcda21b234607
eli shagam <eli.shagam@gmail.com>
2023-06-22 08:15:38 +0300

    new file:   .gitignore 	new file:   README.md 	new file:   components/Footer.js 	new file:   components/Footer.module.css 	new file:   components/Header.js 	new file:   jsconfig.json 	new file:   netlify.toml 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/_app.js 	new file:   pages/index.js 	new file:   public/favicon.ico 	new file:   public/netliheart.svg 	new file:   styles/globals.css Copied files from github.com/codeSTACKr/next-netlify-starter

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">I'm a NEXT.js app running on Netlify!!</p>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
